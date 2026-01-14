f67db9ee519039c4a285f5fb82774d467c3b4a3b
Animes <animesbarman101@gmail.com>
2026-01-14 17:17:12 +0600

    add some

---
+++ src/app/server.ts
@@ -0,0 +1,24 @@
+import { Server } from 'node:http';
+import app from './app'
+
+let server: Server;
+
+
+
+async function startServer(){
+
+     server= app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+
+
+}
+
+
+startServer();
+
+
+
+
+
+
