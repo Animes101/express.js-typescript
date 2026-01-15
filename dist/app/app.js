fd5656d06bc9e7686f2855e20259d2c6417c090a
Animes <animesbarman101@gmail.com>
2026-01-15 12:35:48 +0600

    add some

--- dist/app/app.js
+++ dist/app/app.js
@@ -7,6 +7,33 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+//router 
+const userRouter = express_1.default.Router();
+const studentRouter = express_1.default.Router();
+app.use('/api/v1', userRouter);
+app.use('/api/v1', studentRouter);
+userRouter.get('/users', (req, res) => {
+    console.log('user router called');
+    res.send('user list');
+});
+studentRouter.get('/students', (req, res) => {
+    try {
+        res.send('student list');
+    }
+    catch (err) {
+        res.status(500).send('internal server error');
+    }
+});
+//milleware
+const loggerMiddleware = (req, res, next) => {
+    req.body = { name: 'animes barman' };
+    next();
+};
+app.get('/user', loggerMiddleware, (req, res) => {
+    const name = req.body;
+    console.log(name);
+    res.send(`Hello, ${name.name}`);
+});
 app.get('/', (req, res) => {
     res.send('Hello, World! this is my frist express and type script server');
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,78 @@
-import express from 'express';
+import express, { NextFunction } from 'express';
 import { Request, Response } from 'express';
 
-
-
-
 const app= express();
+
+
 
 
 app.use(express.json());
 app.use(express.text())
 
+
+//router 
+
+const userRouter= express.Router();
+const studentRouter=express.Router();
+
+
+
+app.use('/api/v1', userRouter);
+app.use('/api/v1', studentRouter);
+
+
+
+userRouter.get('/users', (req:Request, res:Response)=>{
+
+    console.log('user router called')
+
+    res.send('user list')
+})
+
+studentRouter.get('/students', (req:Request, res:Response)=>{
+    try{
+        res.send('student list')
+
+    }catch(err){
+        res.status(500).send('internal server error')
+    }
+
+    
+})
+
+
+
+//milleware
+
+const loggerMiddleware= (req:Request, res:Response, next:NextFunction)=>{
+
+
+    req.body={name:'animes barman'};
+
+    next();
+}
+
+
+app.get('/user', loggerMiddleware, (req:Request, res:Response)=>{
+
+
+    const name=req.body
+
+    console.log(name);
+
+    res.send(`Hello, ${name.name}`);
+})
+
+
+
 app.get('/', (req :Request, res:Response) => {
     res.send('Hello, World! this is my frist express and type script server');
 });
+
+
+
+
+
 
 
 export default app;
